ffd61c2db36eea85f471d05cd60fb6ddf640049e
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-09-20 10:41:54 +0100

    refactored comments

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -4,16 +4,17 @@ const AMOUNT = ethers.utils.parseEther("0.02")
 
 /**
  * @notice We use this function to programmatically get WETH by depositing it to the WETH contract
+ * @dev We use the abi and address of the WETH contract to interact with it and call the deposit() function
+ * @dev This function is used in the aaveBorrow.js script
  */
 async function getWeth() {
     const { deployer } = await getNamedAccounts()
-    // Call the deposit function on the weth contract
-    // We need the abi, and contract address: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
     const iWeth = await ethers.getContractAt(
         "IWeth",
         "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
         deployer
     )
+
     const tx = await iWeth.deposit({ value: AMOUNT })
     await tx.wait(1)
     const wethBalance = await iWeth.balanceOf(deployer)
